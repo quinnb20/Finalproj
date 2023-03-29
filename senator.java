fbc35d2d694cbf58ef947bcb3a2ef1532bf374e9
Quinn Bauer <129104619+quinnb20@users.noreply.github.com>
2023-03-29 17:47:41 -0400

    Update senator.java

--- senator.java
+++ senator.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Senator{
 
-
-
-
-
+//read file
+  //identify variables in class (party, state, if leader, etc)
 
 }
+
+public static void main(Strings[] args){
+
+  //create array of the objects ti use in senator search in order to delete as the user uses program
+  
+  boolean menu= false
+  
+  while(menu==false){
+  
+  // gather input on whether user wants to search for a single senator or mult
+    
+    // input = single
+      // ask for name, use for loop for senator and print out information
+    
+    //input = multiple
+      //ask for a narrow down (using variables)
+        //delete in array as user continues
+  
+  }
+
+
+} 
