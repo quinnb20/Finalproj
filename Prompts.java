e429f24fa28c3c565813fcd93908666b6a1d8016
Harper9819 <129111951+Harper9819@users.noreply.github.com>
2023-04-01 16:57:54 -0400

    Create Prompts.java

---
+++ Prompts.java
@@ -0,0 +1,16 @@
+System.out.println("Hello! Welcome to the U.S. Senate Directory");
+System.out.println("What are you searching for today?");
+  
+System.out.println("Are you searching for a single member or multiple members of the Senate?");
+System.out.println("Enter 'S' for single or 'M' for multiple.");
+
+//If single
+
+System.out.println("What is the name of the senator you are looking for?");
+System.out.println("What would you like to know about [name of senator]?");
+System.out.println(menu);
+System.out.println("Enter the number corresponding to your choice: ");
+
+  
+
+                   
