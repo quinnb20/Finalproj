f7b4ff8f7d27a22647500d51a8395fa32e3767ec
Harper9819 <129111951+Harper9819@users.noreply.github.com>
2023-04-03 16:16:26 -0400

    Update senator.java

--- senator.java
+++ senator.java
@@ -6,21 +6,16 @@ public class Senator{
   String state;
   int election_class;
   boolean is_leader;
-  String leadership;
   boolean is_chair;
-  String chairperson;
-  String comm_assignments;
   
-  public Senator(String name, String party, String state, int election_class, boolean is_leader, String leadership, boolean is_chair, String chairperson, String comm_assignments){
+  public Senator(String name, String party, String state, int election_class, boolean is_leader, boolean is_chair){
     this.name = name;
     this.party = party;
     this.state = state;
     this.election_class = election_class;
     this.is_leader = is_leader;
-    this.leadership = leadership;
     this.is_chair = is_chair;
-    this.chairperson = chairperson;
-    this.comm_assignments = comm_assignments;
+
   }
   
   public String getName(){
@@ -33,23 +28,39 @@ public class Senator{
     return election_class;
   }public boolean get_is_leader(){
     return is_leader;
-  }public String getLeadership(){
-    return leadership;
   }public boolean get_is_chair(){
     return is_chair;
-  }public String getChairperson(){
-    return chairperson;
-  }public String getComm_assignments(){
-    return comm_assignments;
   }
     
+  //create a class for assignments separately 
   
   
 
 //read file
   //identify variables in class (party, state, if leader, etc)
+  
 
+}public class Leadership{
+  String name;
+  String position;
+  
+  public Leadership(String name, String position){
+    this.name = name;
+    this.position = position;
+    
+}public class Chairperson{
+  String name;
+  String chairTitle;
+  
+}public class Committee{
+  String name;
+  String comm_1;
+  String comm_2;
+  String comm_3;
+  String comm_4;
+  String comm_5;
 }
+  
 
 public static void main(Strings[] args){
 
