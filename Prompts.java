31c4aa597e942aeb17d31cc858f644ac5e2ca27a
Harper9819 <129111951+Harper9819@users.noreply.github.com>
2023-04-03 14:56:42 -0400

    Update Prompts.java

--- Prompts.java
+++ Prompts.java
@@ -1,6 +1,8 @@
 System.out.println("Hello! Welcome to the U.S. Senate Directory");
 System.out.println("What are you searching for today?");
   
+
+int name_row = 0
 System.out.println("Are you searching for a single member or multiple members of the Senate?");
 System.out.println("Enter 'S' for single or 'M' for multiple.");
 
@@ -17,10 +19,27 @@ if(m_or_s == "S"){
     System.out.println("The Senator [sen_name] does not exist. Please double-check your spelling with the list of Senators found on senate.gov");
   }
   else{ 
+    int add_info = 1;
+    while(add_info ==1){
     //Find index of senator w that name
-    System.out.println("What would you like to know about [name of senator]? Please choose one option at a time");
-    System.out.println(menu);
-    System.out.println("Enter the number corresponding to your choice: ");
+      int senator_index;
+      System.out.println("What would you like to know about [name of senator]? Please choose one option at a time");
+      System.out.println(menu);
+      System.out.println("Enter the number corresponding to your choice: ");
+      Scanner keyboard = new Scanner(System.in);
+      ArrayList<String> removed = new ArrayList<String>;
+      System.out.println("Enter the number corresponding to the information you would like to know: ");
+      int user_choice = keyboard.nextInt();
+      String choice = menu.get(user_choice);
+      removed.add(user_choice);
+      
+      //convert user choice category to index
+      
+      String retrieve_info = list[category_indx][senator_indx]
+        
+      
+    
+      
     
     
     
