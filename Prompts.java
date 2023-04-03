7c516fbf496ef212d3684450af82fb1f56934f0e
Harper9819 <129111951+Harper9819@users.noreply.github.com>
2023-04-03 13:55:00 -0400

    Update Prompts.java

--- Prompts.java
+++ Prompts.java
@@ -4,10 +4,20 @@ System.out.println("What are you searching for today?");
 System.out.println("Are you searching for a single member or multiple members of the Senate?");
 System.out.println("Enter 'S' for single or 'M' for multiple.");
 
-//If single
+String m_or_s = keyboard.nextString();
+if(m_or_s == "S"){
+  //Single
 
-System.out.println("What is the name of the senator you are looking for?");
-System.out.println("What would you like to know about [name of senator]?");
+  System.out.println("What is the name of the senator you are looking for?");
+  String sen_name = keyboard.nextString();
+  public boolean containsName(String userName, ArrayList<String> list){
+    return list.contains(userName)}
+  
+  if(containsName==false){
+    System.out.println("The Senator [sen_name] does not exist. Please double-check your spelling with the list of Senators found on senate.gov");
+  }
+  else{  
+    System.out.println("What would you like to know about [name of senator]? Please choose one option at a time");
 System.out.println(menu);
 System.out.println("Enter the number corresponding to your choice: ");
 
