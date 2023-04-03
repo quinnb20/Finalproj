dec838633b347f41ba302963299b3a7e9fa35438
Harper9819 <129111951+Harper9819@users.noreply.github.com>
2023-04-03 13:26:16 -0400

    Update menu.java

--- menu.java
+++ menu.java
@@ -11,11 +11,17 @@ menu.add("Committee Assignments");
 
 
 //Prints menu and attaches numbers starting at 0 to use indexing to prompt response
-String m[] = menu.toArray(new String[menu.size()]);
-int counter = 0;
-for(String menu_string : m){
-  String str = String.valueOf(counter);
-  System.out.println(str + ".  " + menu_string);
-  counter += 1;
-}System.out.println();
+
+static String printMenu(ArrayList<String> menu_object){
+  String m[] = menu_object.toArray(new String[menu_object.size()]);
+  int counter = 0;
+  for(String menu_string : m){
+    String str = String.valueOf(counter);
+    System.out.println(str + ".  " + menu_string);
+    counter += 1;
+  }System.out.println();
+}
+Scanner keyboard = new Scanner(System.in);
+System.out.println("Enter the number corresponding to the information you would like to know: ");
+int user_choice = keyboard.nextInt()
   
